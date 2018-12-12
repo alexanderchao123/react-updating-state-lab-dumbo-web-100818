3945b37acbdf069ecc2595b7c10f5ad36b85331c
github username <chao.alexander.123@gmail.com>
2018-12-12 15:08:33 -0500

    create components to register click and modify state with nested attributes

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+class DigitalClicker extends Component {
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  increaseClick = () => {
+    this.setState({
+      timesClicked: ++this.state.timesClicked
+    })
+  }
+
+  render () {
+    return (
+      <button onClick={this.increaseClick}>{this.state.timesClicked}</button>
+    )
+  }
+}
+
+export default DigitalClicker
